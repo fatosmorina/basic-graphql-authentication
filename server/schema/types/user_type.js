366c5dc549eb12d1b8598a8492d2fc271ee317dd
fatosmorina <fatosi.morina@gmail.com>
2017-11-08 13:41:11 +0100

    Set UserType attributes

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -5,7 +5,12 @@ const {
 } = graphql;
 
 const UserType = new GraphQLObjectType({
-
+    name: 'UserType',
+    fields: {
+        email: {
+            type: GraphQLString
+        }
+    }
 });
 
 module.exports = UserType;
