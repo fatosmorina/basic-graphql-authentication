aaaaeadb11c19b6589f244af0c5152e91f9412e0
fatosmorina <fatosi.morina@gmail.com>
2017-11-19 18:15:18 +0100

    Create new ApolloClient

--- client/index.js
+++ client/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 
+const client = new ApolloClient({
+  
+});
+
 const Root = () => {
   return (
     <div>
