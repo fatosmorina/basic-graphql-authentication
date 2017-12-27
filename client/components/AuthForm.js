76843b53c75f9818dbafbc0d3b6fcbdaa70ea500
fatosmorina <fatosi.morina@gmail.com>
2017-12-27 20:37:20 +0100

    Declare constructor of AuthForm

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class AuthForm extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { email: '', password: '' };
+    }
+
+    render(){
+        return(
+            <form>
+            </form>
+        );
+    }
 
 }
 
