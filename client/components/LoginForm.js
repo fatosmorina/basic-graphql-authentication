56b983c6a5e194f05212f55ee92e79b527ba1f7e
fatosmorina <fatosi.morina@gmail.com>
2017-12-29 20:52:12 +0100

    Import AuthForm at LoginForm

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -5,6 +5,7 @@ class LoginForm extends Component {
     render(){
         return(
             <div className="container">
+             <h3>Login form</h3>
                 <AuthForm />
             </div>
         );
