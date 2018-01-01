17030617c6a83cf44e301ea3f9b6ba68e8807efa
fatosmorina <fatosi.morina@gmail.com>
2018-01-01 09:10:43 +0100

    Export the Login mutation

--- client/mutations/Login.js
+++ client/mutations/Login.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 
-const mutation = gql`
+export default gql`
     mutation Login($email: String, $password: String){
         login(email: $email, password: $password){
             id
