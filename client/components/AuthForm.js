78736e787ebab58fce41d12b990645bed5cff6f8
fatosmorina <fatosi.morina@gmail.com>
2018-01-03 09:38:01 +0100

    Put error in AuthForm in a particular div

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -23,9 +23,10 @@ class AuthForm extends Component {
                   <div className="input-field">
                     <input placeholder="password" type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value }) } />
                   </div>
-
+                 <div className="errors">
                     { this.props.errors( error => <div key={error}> {error}</div>) }
-                  <button className="btn">Submit</button>
+                 </div>
+                <button className="btn">Submit</button>
                 </form>
             </div>
         );
