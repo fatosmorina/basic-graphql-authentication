8e17bc4b830bc8ddfbd61caee29ede3e0a3d0831
fatosmorina <fatosi.morina@gmail.com>
2018-01-05 09:50:17 +0100

    Add SignupForm component

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 
 
 class SignupForm extends Component{
-
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
 }
 
 export default SignupForm;
