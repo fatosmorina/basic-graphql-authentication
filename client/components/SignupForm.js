1ea30de1079839d5e9c9396d4cb5bcd2094a3dc6
fatosmorina <fatosi.morina@gmail.com>
2018-01-15 11:54:37 +0100

    Remove spaces before and after the content of props

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -24,7 +24,7 @@ class SignupForm extends Component {
         return(
             <div>
                 <h3> Sign up</h3>
-                <AuthForm onSubmit={this.onSubmit.bind(this)} errors={ this.state.errors } />
+                <AuthForm onSubmit={this.onSubmit.bind(this)} errors={this.state.errors} />
             </div>
         )
     }
