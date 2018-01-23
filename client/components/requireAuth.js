e03e3bc0177893e012971e66c9b29a9827634032
fatosmorina <fatosi.morina@gmail.com>
2018-01-23 13:15:59 +0100

    Import CurrentUser at requireAuth

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import query from '../queries/CurrentUser';
 
 class RequireAuth extends Component {
-    
+    componentDidMount(){
+
+    }
 }
 
 export default RequireAuth;
