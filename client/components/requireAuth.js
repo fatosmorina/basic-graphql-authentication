3efae3245c6131228887e6c1dfb401a1f1cb83b3
fatosmorina <fatosi.morina@gmail.com>
2018-01-24 13:18:07 +0100

    Import hashHistory

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class RequireAuth extends Component {
     componentDidMount(){
