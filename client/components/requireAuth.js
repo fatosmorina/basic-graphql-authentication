d3f916484746a6e02e62ad6e4562cce15cba50b0
fatosmorina <fatosi.morina@gmail.com>
2018-01-28 13:23:22 +0100

    Export requireAuth as WrappedComponent

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import query from '../queries/CurrentUser';
-import { hashHistory } from 'react-router';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import query from "../queries/CurrentUser";
+import { hashHistory } from "react-router";
 
-class RequireAuth extends Component {
-    componentDidMount(){
-        if (!this.props.data.loading && !this.props.data.user){
-            hashHistory.push('/login');
-        }
+export default WrappedComponent => {
+  class RequireAuth extends Component {
+    componentDidMount() {
+      if (!this.props.data.loading && !this.props.data.user) {
+        hashHistory.push("/login");
+      }
     }
-}
+  }
+};
 
 graphql(query)(RequireAuth);
